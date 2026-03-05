73705f5a45179439e211c59c015ee88348652c73
Jason Reaume <jreaume5@emich.edu>
2026-03-04 22:30:29 -0500

    Add working prototype for demonstrating nn-Meter functionality.

--- initial_test.py
+++ initial_test.py
@@ -1,10 +1,20 @@
-from nn_meter import load_latency_predictor
+import tf2onnx
+from keras import models, Input, layers
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from nn_meter import load_latency_predictor
+import warnings
+import logging
 
-# 1. Define your architecture
+# Mute scikit-learn version warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Mute nn-Meter info logs and version warnings
+logging.getLogger('nn_meter').setLevel(logging.ERROR)
+
+# Define CNN model architecture
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='leaky_relu', padding='same', input_shape=(32, 32, 3)),
+    Input((32, 32, 3)),
+    layers.Conv2D(32, (3, 3), activation='leaky_relu', padding='same'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='leaky_relu', padding='same'),
     layers.MaxPooling2D((2, 2)),
@@ -16,10 +26,58 @@ model = models.Sequential([
     layers.Dense(10, activation='softmax')
 ])
 
-predictor = load_latency_predictor(predictor_name="cortexA76cpu_tflite21", predictor_version=1.0)
+# Convert TensorFlow Keras model to ONNX format
+# (use a batch size of 1 to simulate single-image edge inference)
+input_signature = [tf.TensorSpec([1, 32, 32, 3], tf.float32, name='x')]
+onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
 
-# 3. Predict latency
-# Note: Ensure the model is passed as the Keras object and type is 'keras'
-latency = predictor.predict(model, model_type='keras')
+"""
+TODO: Change functionality to allow users to enter their own custom 
+model filename for use instead of current hardcoded method. 
 
-print(f"Predicted Latency: {latency} ms")
+TODO: Create function that maps model file extension to variable for 
+model_type parameter used by nnMeterPredictor.
+
+TODO: Add loop to allow the user to test another model or a different
+inference framework if they want to.
+"""
+
+# Save the ONNX file (write binary)
+model_filename = 'custom_model.onnx'
+with open(model_filename, 'wb') as f:
+    f.write(onnx_model.SerializeToString())
+
+inference_frameworks = ['cortexA76cpu_tflite21', 'adreno640gpu_tflite21',
+                        'adreno630gpu_tflite21', 'myriadvpu_openvino2019r2']
+num_frameworks = len(inference_frameworks)
+
+while (True):
+    print(f"Using model: {model_filename}\n")
+    print('Options:')
+    for i, platform in enumerate(inference_frameworks, start=1):
+        print(f"{i}. {platform}")
+
+    # Prompt user for input
+    user_input = input(f"\nEnter a number (1-{num_frameworks}) to "
+                       "select a device inference framework to perform "
+                       "latency prediction on: "
+                       )
+
+    try:
+        # Check if the user entered a non-numeric value or invalid option
+        if (not user_input.isnumeric):
+            raise ValueError
+        user_input = int(user_input)  # typecast input to integer
+        if (user_input < 1 or user_input > num_frameworks):
+            raise ValueError
+        else:
+            break  # valid input = exit the loop
+    except:
+        print("Error: Invalid option.\n")
+        continue
+
+# Predict the inference latency of the model on the device
+predictor = load_latency_predictor(
+    predictor_name=inference_frameworks[user_input - 1], predictor_version=1.0)
+
+latency = predictor.predict('custom_model.onnx', model_type='onnx')

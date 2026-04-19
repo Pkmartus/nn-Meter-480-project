8543e8c9ae116961043939b30160a785e9028e27
pkmartus <pkmartus@gmail.com>
2026-04-18 21:55:31 -0400

    updated names of files and notes

---
+++ detect_fusion_rules.py
@@ -0,0 +1,28 @@
+from nn_meter.builder import builder_config
+from nn_meter.builder.backends import connect_backend
+from nn_meter.builder import profile_models
+from nn_meter.builder.backend_meta.fusion_rule_tester import generate_testcases
+from nn_meter.builder.backend_meta.fusion_rule_tester import detect_fusion_rule
+import monkeypatch # type: ignore # pylint: disable=unused-import
+
+
+#initialize builder
+builder_config.init(
+    workspace_path="./z839"
+)
+
+#create testcases
+origin_testcases = generate_testcases()
+
+#connect to backend
+backend = connect_backend(backend_name='tflite_cpu')
+
+#run testcases and collect profiling results
+profiled_results = profile_models(backend, origin_testcases, mode='ruletest')
+
+#determine fusion rools from results
+detected_results = detect_fusion_rule(profiled_results)
+
+
+
+
